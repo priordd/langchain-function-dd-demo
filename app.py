d58964fb32dd7c7bef028bc59dfce548d96eb360
priordd <rodrigo.prior@datadoghq.com>
2025-04-20 22:56:18 -0300

    human readable response with functions

--- app.py
+++ app.py
@@ -3,14 +3,15 @@
 import os
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage
+from langchain_core.tools import tool
+
 
 load_dotenv()
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 
 
-# The function name, type hints, and docstring are all part of the tool
-# schema that's passed to the model. Defining good, descriptive schemas
-# is an extension of prompt engineering and is an important part of
-# getting models to perform well.
+@tool
 def add(a: int, b: int) -> int:
     """Add two integers.
 
@@ -21,6 +22,7 @@ def add(a: int, b: int) -> int:
     return a + b
 
 
+@tool
 def multiply(a: int, b: int) -> int:
     """Multiply two integers.
 
@@ -32,10 +34,19 @@ def multiply(a: int, b: int) -> int:
 
 
 tools = [add, multiply]
-
-
-llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 llm_with_tools = llm.bind_tools(tools)
-query = "What is 3 * 12?"
-query = llm_with_tools.invoke(query)
-print(query.tool_calls)
+
+query = "What is 3 * 12? Also, what is 11 + 49?"
+messages = [HumanMessage(query)]
+ai_msg = llm_with_tools.invoke(messages)
+# print(ai_msg.tool_calls)
+messages.append(ai_msg)
+
+for tool_call in ai_msg.tool_calls:
+    selected_tool = {"add": add, "multiply": multiply}[tool_call["name"].lower()]
+    tool_msg = selected_tool.invoke(tool_call)
+    messages.append(tool_msg)
+
+response = llm_with_tools.invoke(messages)
+
+print(response.content)
