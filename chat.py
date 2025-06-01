95abe129d1924910caeedf64cd9fe8345b33a72d
priordd <rodrigo.prior@datadoghq.com>
2025-06-01 16:20:50 -0300

    added langgraph to cl + dd

--- chat.py
+++ chat.py
@@ -3,42 +3,126 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from typing import Literal
+from langchain_core import tools
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema import StrOutputParser
-from langchain.schema.runnable import Runnable
+from langgraph.prebuilt import ToolNode
 from langchain.schema.runnable.config import RunnableConfig
-from typing import cast
+from langchain_core.messages import HumanMessage
 
 import chainlit as cl
 
+from ddtrace.llmobs import LLMObs
+from ddtrace.llmobs import decorators
+from ddtrace.llmobs.decorators import embedding, llm, retrieval, workflow, tool
 
-@cl.on_chat_start
-async def on_chat_start():
-    model = ChatOpenAI(streaming=True)
-    prompt = ChatPromptTemplate.from_messages(
+LLMObs.enable(
+    api_key=os.getenv("DD_API_KEY"),
+    site=os.getenv("DD_SITE"),
+    ml_app=os.getenv("DD_LLMOBS_ML_APP"),
+    agentless_enabled=0,
+    env=os.getenv("DD_ENV"),
+    service=os.getenv("DD_SERVICE"),
+)
+
+
+@tools.tool
+def get_weather(city: Literal["nyc", "sf"]):
+    """Use this to get weather information."""
+    if city == "nyc":
+        return "It might be cloudy in nyc"
+    elif city == "sf":
+        return "It's always sunny in sf"
+    else:
+        raise AssertionError("Unknown city")
+
+
+tools = [get_weather]
+model = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+final_model = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+model = model.bind_tools(tools)
+# NOTE: this is where we're adding a tag that we'll can use later to filter the model stream events to only the model called in the final node.
+# This is not necessary if you call a single LLM but might be important in case you call multiple models within the node and want to filter events
+# from only one of them.
+final_model = final_model.with_config(tags=["final_node"])
+tool_node = ToolNode(tools=tools)
+
+from typing import Annotated
+from typing_extensions import TypedDict
+
+from langgraph.graph import END, StateGraph, START
+from langgraph.graph.message import MessagesState
+from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage
+
+
+def should_continue(state: MessagesState) -> Literal["tools", "final"]:
+    messages = state["messages"]
+    last_message = messages[-1]
+    # If the LLM makes a tool call, then we route to the "tools" node
+    if last_message.tool_calls:
+        return "tools"
+    # Otherwise, we stop (reply to the user)
+    return "final"
+
+
+def call_model(state: MessagesState):
+    messages = state["messages"]
+    response = model.invoke(messages)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+
+def call_final_model(state: MessagesState):
+    messages = state["messages"]
+    last_ai_message = messages[-1]
+    response = final_model.invoke(
         [
-            (
-                "system",
-                "You're a very knowledgeable Principal Site Reliability Engineer (SRE) at OpenAI would be a highly experienced and influential leader, focusing on building and maintaining the reliability and scalability of OpenAI's AI infrastructure. This role involves deep technical expertise, strong leadership, and the ability to mentor and guide other engineers.",
-            ),
-            ("human", "{question}"),
+            SystemMessage("Rewrite this in the voice of Al Roker"),
+            HumanMessage(last_ai_message.content),
         ]
     )
-    runnable = prompt | model | StrOutputParser()
-    cl.user_session.set("runnable", runnable)
+    # overwrite the last AI message from the agent
+    response.id = last_ai_message.id
+    return {"messages": [response]}
+
+
+builder = StateGraph(MessagesState)
+
+builder.add_node("agent", call_model)
+builder.add_node("tools", tool_node)
+# add a separate final node
+builder.add_node("final", call_final_model)
+
+builder.add_edge(START, "agent")
+builder.add_conditional_edges(
+    "agent",
+    should_continue,
+)
+
+builder.add_edge("tools", "agent")
+builder.add_edge("final", END)
+
+graph = builder.compile()
 
 
 @cl.on_message
-async def on_message(message: cl.Message):
-    runnable = cast(Runnable, cl.user_session.get("runnable"))  # type: Runnable
+@workflow
+async def on_message(msg: cl.Message):
+    config = {"configurable": {"thread_id": cl.context.session.id}}
+    cb = cl.LangchainCallbackHandler()
+    final_answer = cl.Message(content="")
 
-    msg = cl.Message(content="")
-
-    async for chunk in runnable.astream(
-        {"question": message.content},
-        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    for msg, metadata in graph.stream(
+        {"messages": [HumanMessage(content=msg.content)]},
+        stream_mode="messages",
+        config=RunnableConfig(callbacks=[cb], **config),
     ):
-        await msg.stream_token(chunk)
+        if (
+            msg.content
+            and not isinstance(msg, HumanMessage)
+            and metadata["langgraph_node"] == "final"
+        ):
+            await final_answer.stream_token(msg.content)
 
-    await msg.send()
+    await final_answer.send()
